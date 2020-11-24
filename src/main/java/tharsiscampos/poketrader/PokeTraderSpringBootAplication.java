2fa76602a1b5deff37aed28bb16d588a85dbe184
Tharsis Campos <tharsis@maximizesistemas.com.br>
2020-11-23 22:02:25 -0300

    inicialização bd

--- src/main/java/tharsiscampos/poketrader/PokeTraderSpringBootAplication.java
+++ src/main/java/tharsiscampos/poketrader/PokeTraderSpringBootAplication.java
@@ -1,5 +1,8 @@
 package tharsiscampos.poketrader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,10 +13,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import me.sargunvohra.lib.pokekotlin.client.PokeApi;
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+import tharsiscampos.poketrader.entity.Pessoa;
+import tharsiscampos.poketrader.repo.PessoaREPO;
 
 @SpringBootApplication
 @EnableCaching
 public class PokeTraderSpringBootAplication {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(PokeTraderSpringBootAplication.class, args);
 	}
@@ -25,4 +33,23 @@ public class PokeTraderSpringBootAplication {
 	@Bean @Primary public PokeApi pokeApi() {
         return new PokeApiClient();
     }
+	
+	@Bean public CommandLineRunner loadData(PessoaREPO pessoaREPO) {
+	    return (args) -> {
+	        if (pessoaREPO.findById(PessoaREPO.ID_PESSOA_A).isEmpty()) {
+	        	Pessoa a = new Pessoa();
+	        	a.setNome("Aldanice");
+	        	a.setId(1);
+	        	pessoaREPO.save(a);
+	        	logger.info("Pessoa Aldanice criada.");
+	        }
+	        if (pessoaREPO.findById(PessoaREPO.ID_PESSOA_B).isEmpty()) {
+	        	Pessoa b = new Pessoa();
+	        	b.setNome("Bernardino");
+	        	b.setId(2);
+	        	pessoaREPO.save(b);
+	        	logger.info("Pessoa Bernardino criada.");
+	        }
+	    };
+	}
 }
